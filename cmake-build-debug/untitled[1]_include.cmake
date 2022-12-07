cd85827fdc3923d7c128682b4bdbc0743dbf2109
BaXXeRFeeD <so_asarkis@vk.com>
2022-12-08 01:57:29 +0300

    ver 4

---
+++ cmake-build-debug/untitled[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "C:/Users/vova_/CLionProjects/untitled/cmake-build-debug/untitled[1]_tests.cmake")
+  include("C:/Users/vova_/CLionProjects/untitled/cmake-build-debug/untitled[1]_tests.cmake")
+else()
+  add_test(untitled_NOT_BUILT untitled_NOT_BUILT)
+endif()
